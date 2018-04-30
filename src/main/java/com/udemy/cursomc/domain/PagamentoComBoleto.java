c00a9f64fda3528cf34aa0ac74a8cc2d19195ebf
Carlos Gustavo Etchepare <cgetchepare@gmail.com>
2018-04-30 17:08:21 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/udemy/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/udemy/cursomc/domain/PagamentoComBoleto.java
@@ -4,30 +4,33 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.udemy.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
-public class PagamentoComBoleto extends Pagamento{
+public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Date dataVencimento;
 	private Date dataPagamento;
-	
+
 	public PagamentoComBoleto() {
-		
+
 	}
 
-	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento,
+			Date dataPagamento) {
 		super(id, estado, pedido);
 		this.dataVencimento = dataVencimento;
 		this.dataPagamento = dataPagamento;
-	
 	}
 
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	public Date getDataVencimento() {
 		return dataVencimento;
 	}
 
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	public void setDataVencimento(Date dataVencimento) {
 		this.dataVencimento = dataVencimento;
 	}
@@ -39,9 +42,5 @@ public class PagamentoComBoleto extends Pagamento{
 	public void setDataPagamento(Date dataPagamento) {
 		this.dataPagamento = dataPagamento;
 	}
-
-	
-	
-	
 
 }
