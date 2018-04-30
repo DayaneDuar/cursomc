e16e6adb5887438492253481a085be034e541072
Carlos Gustavo Etchepare <cgetchepare@gmail.com>
2018-04-30 14:50:47 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/udemy/cursomc/domain/Cliente.java
+++ src/main/java/com/udemy/cursomc/domain/Cliente.java
@@ -38,6 +38,9 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 		
 	}
@@ -101,6 +104,7 @@ public class Cliente implements Serializable {
 		this.tipo = tipo;
 	}
    */
+	
 	public List<Endereco> getEnderecos() {
 		return enderecos;
 	}
@@ -115,6 +119,18 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	public void setTipo(Integer tipo) {
+		this.tipo = tipo;
 	}
 
 	@Override
@@ -141,8 +157,4 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-
-	
 }
